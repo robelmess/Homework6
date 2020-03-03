a38d628ac4c18a0a90173fca4e2defec01de5b49
robelmess <robelmessele1@gmail.com>
2020-03-02 16:08:45 -0800

    Fixed indexes but search does not work

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
     // do not change the signature of index()
-    private Map<String, ArrayList<ArrayList<Integer>>> indexes;
+    private Map<String, List<List<Integer>>> indexes;
 
     // disable default constructor
 
@@ -20,16 +21,25 @@ public class MyMiniSearchEngine {
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-
+        indexes=new HashMap<>();
         for(int i=0;i<texts.size();i++)
         {
             String[] arr= texts.get(i).split(" ");
             //every individual word
             for(int j=0;j<arr.length;j++)
             {
-                indexes.put();
+                List<List<Integer>> outer=new ArrayList();
+                for (int k = 0; k < texts.size(); k++) {
+                    outer.add(new ArrayList<Integer>());
+                }
+                if(!indexes.containsKey(arr[j]))
+                {
+                    indexes.put(arr[j],outer);
+                }
+                indexes.get(arr[j]).get(i).add(j);
             }
         }
+        System.out.println(indexes.toString());
     }
 
     // search(key) return all the document ids where the given key phrase appears.
@@ -37,6 +47,12 @@ public class MyMiniSearchEngine {
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
         // homework
+        for(int i=0;i<indexes.size();i++){
+            if(indexes.get(i).contains(keyPhrase)))
+            {
+                return indexes.get(i);
+            }
+        }
         return new ArrayList<>(); // place holder
     }
 }
